62d4d69807d918d3b5f1f5ecf8eb595f23a09f3b
Stella Sotos <stellasotos@Stellas-MacBook-Pro.local>
2018-01-28 00:41:08 -0500

    begin data analysis

--- report.php
+++ report.php
@@ -16,16 +16,36 @@
     $time=$_POST['time-entry'];
     $type = $_POST['assault-types'];
     $known = $_POST['yes-no'];
-    echo $date, $month, $year, $type ,$known;
+    //echo $date, $month, $year, $type ,$known;
 
-    $sql = "INSERT INTO uvaData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    if($_POST['school-name']=='University of Virginia')
+        $sql = "INSERT INTO uvaData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='Virginia Tech')
+        $sql = "INSERT INTO vtData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='Georgetown University')
+        $sql = "INSERT INTO gtData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='University of Pennsylvania')
+        $sql = "INSERT INTO pennData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='Harvard College')
+        $sql = "INSERT INTO harData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='Yale College')
+        $sql = "INSERT INTO yaData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else if($_POST['school-name']=='Cornell University')
+        $sql = "INSERT INTO coData VALUES ('$date','$month','$year','$time,','$type','$known')";
+    else
+        $sql = "INSERT INTO broData VALUES ('$date','$month','$year','$time,','$type','$known')";
+
+
 
     if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
+        //echo "New record created successfully";
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     mysqli_close($conn);
+    echo "<script>
+    window.location = 'http://weknow.report/school.html';
+    </script>"
 
 ?>
